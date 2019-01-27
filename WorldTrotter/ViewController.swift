4d33e7b92f4fd64ce7414fae1ab07a7772e29ea6
Manij <manij07@hotmail.co.uk>
2019-01-27 22:35:46 +0000

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Manij Basnet on 27/01/2019.
+//  Copyright © 2019 Basnet Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
